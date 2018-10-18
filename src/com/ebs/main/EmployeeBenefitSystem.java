9dcd1bb20d0f1566fde7b0a92447c34bdbdc6e31
faisalkhan <faisalkhan@csu.fullerton.edu>
2018-10-17 17:53:47 -0700

    Db Connection with h2 done

--- src/com/ebs/main/EmployeeBenefitSystem.java
+++ src/com/ebs/main/EmployeeBenefitSystem.java
@@ -1,8 +1,13 @@
 package com.ebs.main;
 
+import com.ebs.db.DbConfig;
+
 public class EmployeeBenefitSystem {
 
 	public static void main(String[] args) {
 		System.out.println("Employee Benefit System:");
+		
+		DbConfig dbConfig = new DbConfig();
+		dbConfig.connect();
 	}
 }
