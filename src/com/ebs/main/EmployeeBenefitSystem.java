1fc5e3fa710562672f6fc840910f6036171e36e3
faisalkhan <faisalkhan@csu.fullerton.edu>
2018-10-17 18:00:24 -0700

    changes to Main

--- src/com/ebs/main/EmployeeBenefitSystem.java
+++ src/com/ebs/main/EmployeeBenefitSystem.java
@@ -7,6 +7,9 @@ public class EmployeeBenefitSystem {
 	public static void main(String[] args) {
 		System.out.println("Employee Benefit System:");
 		
+		/**
+		 * Test DB here
+		 */
 		DbConfig dbConfig = new DbConfig();
 		dbConfig.connect();
 	}
